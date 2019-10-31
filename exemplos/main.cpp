d2fe169a21bd00c7b1705948183c7cfda60218ec
Eduardo da Silva <1910485@ifes.edu.br>
2019-10-31 16:27:17 -0300

    Inserção de exemplos do operador new

--- exemplos/main.cpp
+++ exemplos/main.cpp
@@ -8,10 +8,30 @@ unsigned long long int fatorial (unsigned long long int n){
 
 
 int main () {
+    cout << "************** Recursividade **************" << endl;
     unsigned long long int n, fat;
     cout << "Informe o valor de n: ";
     cin >> n;
     fat = fatorial(n);
     cout << "O fatorial e : " << fat << endl;
+    cout << "*****************************************" << endl;
+
+    cout << "************** Alocacao Dinamica com operador new **************" << endl;
+    float *notas;
+    int quantidade = 0, i;
+    cout << "Informe quantas notas deseja armazenar: ";
+    cin >> quantidade;
+    notas = new float[quantidade];
+
+    for(i=0; i< quantidade; i++){
+        cout << "Informe a nota " << i+1 << " ";
+        cin >> notas[i];
+    }
+
+    cout << "As notas digitadas foram: ";
+    for(i=0; i< quantidade; i++){
+        cout << "  " << notas[i];
+    }
+    cout << endl;
 return 0;
 }
